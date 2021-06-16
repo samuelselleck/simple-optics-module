51532cabac0eb1dc576c301122f109c236dbd134
Samuel Selleck <sellecksamuel@gmail.com>
2021-06-16 15:49:49 +0200

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+    import OpticsObject from "./_components/OpticsObject.svelte";
+    let opticsObjects = [{pos: {x: 500, y: 500}, height: 300, focal: 500}]
+
+    function key(event) {
+        console.log(opticsObjects)
+        opticsObjects = [...opticsObjects, {pos: {x: 500, y: 500}, height: 300, focal: 500}]
+    }
+</script>
+
+<div class="fill">
+<svg class="canvas fill" on:mouseup={key} xmlns="http://www.w3.org/2000/svg">
+    {#each opticsObjects as properties}
+        <OpticsObject {properties}/>
+	{/each}
+</svg>
+</div>
+
+<style>
+    .canvas {
+        background: black;
+    }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@sveltejs/kit').Config} */
+
+const config = {
+	kit: {
+		target: '#svelte'
+	}
+};
+
+export default config;
